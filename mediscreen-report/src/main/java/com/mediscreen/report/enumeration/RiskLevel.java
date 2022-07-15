3eec60d54bc4e778ae48daed71d153e200a059f7
Johan Nadaud <johan.nadaud.pro@gmail.com>
2022-07-15 12:34:15 +0200

    Add mediscreen-report enum RiskLevel Add mediscreen-report Report model

---
+++ mediscreen-report/src/main/java/com/mediscreen/report/enumeration/RiskLevel.java
@@ -0,0 +1,8 @@
+package com.mediscreen.report.enumeration;
+
+public enum RiskLevel {
+    NONE,
+    BORDERLINE,
+    IN_DANGER,
+    EARLY_ONSET
+}
---
+++ mediscreen-report/src/main/java/com/mediscreen/report/model/Report.java
@@ -0,0 +1,34 @@
+package com.mediscreen.report.model;
+
+public class Report {
+
+   private Patient patient;
+
+   private int age;
+
+   private String riskLevel;
+
+   public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRiskLevel() {
+        return riskLevel;
+    }
+
+    public void setRiskLevel(String riskLevel) {
+        this.riskLevel = riskLevel;
+    }
+}
